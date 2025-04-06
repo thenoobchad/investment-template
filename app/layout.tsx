84b7c303dfa9819ddc4548b5e99bd3fce3acfc30
thenoobchad <noobchaddy@gmail.com>
2025-04-07 00:30:32 +0100

    nav bar

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,9 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { ReactNode } from "react";
+import Link from "next/link";
+import { Button } from "@/components/ui/button";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -17,16 +20,45 @@ export const metadata: Metadata = {
   description: "financially driving digitalization",
 };
 
-export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
+const links = [
+  {
+    name: "Home",
+    pathname: "/",
+  },
+  {
+    name: "About us",
+    pathname: "/about-us",
+  },
+  {
+    name: "Packages",
+    pathname: "/packages",
+  },
+  {
+    name: "Dashboard",
+    pathname: "/dashboard",
+  },
+];
+
+export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="en">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <div className="w-full! flex justify-center">
+          <div className="container w-full h-30 p-4 flex flex-row justify-between items-center font-[family-name:var(--font-geist-sans)]">
+            <div>Investpro</div>
+            <div className="flex flex-row gap-6 items-center">
+              {links.map((link, index) => (
+                <Link key={link.name + "-" + index} href={link.pathname}>
+                  {link.name}
+                </Link>
+              ))}
+
+              <Button>Login in</Button>
+            </div>
+          </div>
+        </div>
         {children}
       </body>
     </html>
